0f05e6c03d079a16c2bbebcc5b1ac182ca9fb4f4
nellace <nellace@qq.com>
2015-07-03 14:31:35 +0800

    Initial Commit

---
+++ Snow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Snow
+//
+//  Created by WO on 15/7/3.
+//  Copyright (c) 2015年 sg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
